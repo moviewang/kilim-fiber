030a19c9fdc563c17ecb13d2ba1acfba09eafb93
Tao Wen <taowen@gmail.com>
2014-09-26 14:23:02 +0800

    remove non fiber code

--- src/kilim/Generator.java
+++ src/kilim/Generator.java
@@ -43,7 +43,7 @@ public class Generator<T> extends Task implements Iterator<T>, Iterable<T> {
         if (nextVal == null) {
             if (isDone())
                 return false;
-            _runExecute(null);
+            _runExecute();
             return nextVal != null;
         } else {
             return true;
@@ -60,7 +60,7 @@ public class Generator<T> extends Task implements Iterator<T>, Iterable<T> {
         if (isDone()) {
             throw new NoSuchElementException();
         }
-        _runExecute(null);
+        _runExecute();
         ret = nextVal;
         nextVal = null;
         return ret;
